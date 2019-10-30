1f626cd463db986efe83d4ef1738abeaab5c1fcf
huseyintasci <huseyintasci@yandex.com>
2019-10-30 16:40:59 -0700

    Scrapind data

--- Server.js
+++ Server.js
@@ -9,15 +9,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("Public"));
 
+
+//Mongo DB Connection
 var mongodb_url = process.env.mongodb_url || "mongodb://localhost:27017/dnews_db";
-mongoose.connect(mongodb_url, {useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(mongodb_url, {useUnifiedTopology: true});
 
 
 app.get("/articles", function(req, res) {
   db.Article.find({})
   .then(function(dbArticle) {
       res.json(dbArticle);
-      console.log(dbArticle)
+    
   })
   .catch(function(err) { 
       res.json(err);
@@ -25,10 +27,10 @@ app.get("/articles", function(req, res) {
 });
 
 app.get("/scrape", function(req, res) {
-  axios.get("https://www.reddit.com/r/technology/").then(function(response) {
+  axios.get("https://old.reddit.com/r/webdev").then(function(response) {
   var newArticleCount = 0; 
   var $ = cheerio.load(response.data); 
-console.log(response);
+ 
   $("p.title").each(function(i, element) {
       var result = {};
       result.title = $(this).text();
@@ -59,7 +61,7 @@ app.get("/comments/:id", function(req, res) {
       res.json(dbArticle);
   })
   .catch(function(err) {
-      res.json(err);
+      res.json(err); 
   });
 });
 
@@ -93,5 +95,5 @@ db.Comment.findOneAndDelete({ _id: req.params.id })
 var PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
-  });
+  });  
   
